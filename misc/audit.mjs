02b1a53d632652a06eb5973d683dfd7e3537ac13
Nicolas Peltier <npeltier@adobe.com>
2024-07-09 14:28:44 +0200

    MWPW-153658 move usage to readme

--- misc/audit.mjs
+++ misc/audit.mjs
@@ -1,27 +1,6 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
 
-/**
- * USAGE:
- *
- * for one page or two or more 
- * node audit.mjs https://www.adobe.com/page1.html https://www.adobe.com/page2.html
- *
- * for a sitemap or more
- * node audit.mjs https://www.adobe.com/cc-shared/assets/sitemap.xml
- * 
- * either case output will be entered as csv in /tmp/audit.csv, you can set a different output file with -f parameter
-
- * for a full configured set of URLs and/or sitemap you can use -m parameter with a manifest composed of URLs/Sitemap 
- * separated by new lines character
- * 
- * for faster execution, buffer size (number of parallel page being audited) can be increased but will impact your connection.
- * with parameter -b (you can move it lower to the default that is 100).
- * 
- * so typical execution could be 
- * `node audit.mjs -b 50 -f ~/Documents/audit.csv -m ./audit-manifest.txt`
- */
-
 const BUFFER_SIZE = 100;
 const EXCERPT_SIZE = 30;
 const BUFFER_ARG = '-b';

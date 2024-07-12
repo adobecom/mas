1c40f3fe9baab4fb790b7a87534ee1bb02205800
Axel Cureno Basurto <axel@adobe.com>
2024-07-11 18:17:56 -0700

    CLS

--- web-components/test/static/merch-card/merch-card-static.js
+++ web-components/test/static/merch-card/merch-card-static.js
@@ -13,32 +13,3 @@ const config = () => ({
     commerce: { 'commerce.env': 'PROD' },
     locale: { prefix: locale },
 });
-
-export const appendMiloStyles = () => {
-    const params = new URLSearchParams(window.location.search);
-    let milolibs = params.get('milolibs') ?? 'main--milo--adobecom';
-    if (milolibs === 'local') {
-        milolibs = 'http://localhost:6456';
-    }
-    const customStyles = document.querySelector('style');
-    let style = document.createElement('link');
-    style.rel = 'stylesheet';
-    style.href = `https://${milolibs}.hlx.page/libs/styles/styles.css`;
-    document.head.insertBefore(style, customStyles);
-
-    const loadAsyncCSS = (href) => {
-        const link = document.createElement('link');
-        link.rel = 'preload';
-        link.as = 'style';
-        link.href = href;
-        link.onload = () => (link.rel = 'stylesheet');
-        document.head.appendChild(link);
-    };
-
-    loadAsyncCSS(`https://${milolibs}.hlx.page/libs/styles/merch.css`);
-    loadAsyncCSS(`https://${milolibs}.hlx.page/libs/styles/merch-card.css`);
-};
-
-(async () => {
-    await appendMiloStyles();
-})();

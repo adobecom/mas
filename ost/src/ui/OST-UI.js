d21a40fe0c60b46d44460b6adecaf2db688aebff
Rahul Gupta <rahulgupta999@gmail.com>
2024-08-07 16:56:06 +0530

    Add OST-UI

---
+++ ost/src/ui/OST-UI.js
@@ -0,0 +1,51 @@
+import { html, LitElement, nothing } from 'lit';
+import { Reaction } from 'mobx';
+import { MobxReactionUpdateCustom } from '@adobe/lit-mobx/lib/mixin-custom.js';
+
+class OST-UI extends MobxReactionUpdateCustom(LitElement, Reaction) {
+    TAG_NAME = 'ost-component';
+
+    static properties = {
+        prodCode: { type: Number},
+        entitlementInfo: {
+            planType: { type: String },
+            customerSegment: { type: String },
+            offerType: { type: String },
+            marketSegment: { type: String },
+            countryCode: { type: String }
+        },
+        templateInfo: {
+            type: { type: String },
+            priceInfo: { 
+                disableOptions: { type: Array },
+                priceTypes: { type: Array },
+
+             },
+            checkoutInfo: {
+                checkoutType: { type: String },
+                workflowStep: { type: String },
+                ctaText: { type: String }
+            },
+
+        }
+    };
+
+    constructor() {
+        super();
+    }
+
+    renderHeader() {
+        return html`<div class="ost-ui__header">
+            /div>`
+    }
+
+    renderContent() {
+        return html`<div class="ost-ui__content">
+        </div>`;
+    }
+
+    render() {
+        return html`<div class="ost-ui">
+            ${this.renderHeader()} ${this.renderContent()}
+        </div>`;
+    }

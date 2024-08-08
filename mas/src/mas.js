408f40fb23290cd85708e9a79709e82656bedb1e
Axel Cureno Basurto <axel@adobe.com>
2024-08-08 10:16:19 -0700

    review comments addressed

--- mas/src/mas.js
+++ mas/src/mas.js
@@ -8,37 +8,15 @@ const features = searchParams.get('features');
 const envName = isStage ? 'stage' : 'prod';
 const commerceEnv = isStage ? 'STAGE' : 'PROD';
 
-function loadScript(url) {
-    return new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        script.src = url;
-        script.onload = () => resolve();
-        script.onerror = () =>
-            reject(new Error(`Failed to load script: ${url}`));
-        document.head.appendChild(script);
-    });
-}
-
 const config = () => ({
     env: { name: envName },
     commerce: { 'commerce.env': commerceEnv },
     locale: { prefix: locale },
 });
 
-(async () => {
-    try {
-        await loadScript(
-            'https://main--milo--adobecom.hlx.page/libs/deps/mas/commerce.js',
-        );
-        if (typeof init === 'function') {
-            init(config);
-        } else {
-            console.error('init function is not available.');
-        }
-    } catch (error) {
-        console.error(error);
-    }
-})();
+const { init } = import('https://stage.adobe.com/libs/deps/mas/commerce.js');
+
+init(config);
 
 if (features.includes('merch-card')) {
     import(

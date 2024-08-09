c284814447f8746ed026df5d9e08986222e5dc44
Axel Cureno Basurto <axel@adobe.com>
2024-08-09 12:21:55 -0700

    more lint fixes

--- ost/src/service/NetworkService.js
+++ ost/src/service/NetworkService.js
@@ -1,12 +1,11 @@
 import { computePlanType } from '../utils/Utils.js';
 
 export class NetworkService {
-
     constructor(accessToken, apiKey) {
         this.accessToken = accessToken;
         this.apiKey = apiKey;
     }
-    
+
     // In the long run, we should get rid of loadScript approach and load the list of products 
     // from Sharepoint or Odin and have proper CORS support and use fetch
     loadScript(src) {
@@ -19,31 +18,32 @@ export class NetworkService {
                 script.setAttribute('type', 'text/javascript');
                 head.append(script);
             }
-    
+
             const onScriptLoad = () => {
                 script.removeEventListener('load', onScriptLoad);
                 script.removeEventListener('error', onScriptError);
                 resolve(script);
             };
-    
+
             const onScriptError = () => {
                 script.removeEventListener('load', onScriptLoad);
                 script.removeEventListener('error', onScriptError);
                 reject(new Error(`error loading script: ${src}`));
             };
-    
+
             script.addEventListener('load', onScriptLoad);
             script.addEventListener('error', onScriptError);
         });
     }
 
     async fetchProducts() {
-        const PRODUCTS_ENDPOINT = 'https://www.stage.adobe.com/special/tacocat/products.js';
-    
+        const PRODUCTS_ENDPOINT =
+            'https://www.stage.adobe.com/special/tacocat/products.js';
+
         if (window?.tacocat?.products) {
             return Object.entries(window.tacocat.products);
         }
-    
+
         const script = await this.loadScript(PRODUCTS_ENDPOINT);
         if (script) {
             return Object.entries(window.tacocat.products);
@@ -53,8 +53,9 @@ export class NetworkService {
     }
 
     async fetchCountries() {
-        const COUNTRIES_ENDPOINT = 'https://countries-stage.adobe.io/v2/countries?api_key=dexter-commerce-offers';
-    
+        const COUNTRIES_ENDPOINT =
+            'https://countries-stage.adobe.io/v2/countries?api_key=dexter-commerce-offers';
+
         try {
             const response = await fetch(COUNTRIES_ENDPOINT);
             const data = await response.json();
@@ -67,27 +68,31 @@ export class NetworkService {
             console.error('Error fetching countries', error);
         }
     }
-    
+
     async fetchPaginatedOffers(allProducts, landscape, locale, page = 0) {
         const KEY = {
             PA: 'product_arrangement_code',
         };
         const [, country] = locale.split('_');
         const OFFERS_ENDPOINT = `https://aos.adobe.io/offers?country=${country}&merchant=ADOBE&service_providers=MERCHANDISING&locale=${locale}&api_key=YOUR_API_KEY&landscape=${landscape}&page_size=100&page=${page}`;
-    
+
         try {
             const response = await fetch(OFFERS_ENDPOINT);
             if (!response.ok) {
-                throw new Error(`Network response failed with status: ${response.statusText}`);
+                throw new Error(
+                    `Network response failed with status: ${response.statusText}`,
+                );
             }
-    
+
             let offers;
             try {
                 offers = await response.json();
             } catch (error) {
-                throw new Error('Failed to parse JSON response, error is: ' + error.message);
+                throw new Error(
+                    'Failed to parse JSON response, error is: ' + error.message,
+                );
             }
-    
+
             console.log(`received ${landscape} - ${page}`);
             if (offers && offers.length > 0) {
                 const products = allProducts[landscape];
@@ -112,12 +117,17 @@ export class NetworkService {
                         );
                     }
                 }
-                return await this.fetchPaginatedOffers(allProducts, landscape, locale, ++page);
+                return await this.fetchPaginatedOffers(
+                    allProducts,
+                    landscape,
+                    locale,
+                    ++page,
+                );
             } else {
                 console.log(
                     `collected ${
                         Object.entries(allProducts[landscape]).length
-                    } products for ${landscape}`
+                    } products for ${landscape}`,
                 );
             }
         } catch (error) {

c2272383cccdd912f2d73d53aa2af3ff1a1ec54c
Axel Cureno Basurto <axel@adobe.com>
2024-10-04 14:11:43 -0700

    MWPW-158473: Top navigation wc

---
+++ studio/src/top-nav.js
@@ -0,0 +1,80 @@
+import { LitElement, html, css } from 'lit';
+
+class TopNav extends LitElement {
+    static get styles() {
+        return css`
+            :host {
+                display: block;
+                width: 100%;
+            }
+            sp-top-nav {
+                width: 100%;
+            }
+
+            sp-top-nav-item {
+                margin-inline-end: auto;
+                margin-inline-start: 20px;
+            }
+
+            sp-top-nav-item.logo {
+                color: #eb1000;
+            }
+
+            sp-top-nav-item strong {
+                font-size: 21px;
+                font-weight: 800;
+                line-height: 20px;
+                vertical-align: top;
+                padding-inline-start: 5px;
+            }
+            sp-top-nav-item[placement='bottom-end'] {
+                margin-inline-end: 20px;
+            }
+        `;
+    }
+
+    render() {
+        return html`
+            <sp-top-nav>
+                <sp-top-nav-item
+                    class="logo"
+                    size="l"
+                    href="#"
+                    label="Home"
+                    quiet
+                >
+                    <svg
+                        version="1.1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        x="0"
+                        y="0"
+                        viewBox="0 0 30 26"
+                        width="24px"
+                        xml:space="preserve"
+                        role="img"
+                        aria-label="Adobe"
+                    >
+                        <path
+                            fill="#FA0F00"
+                            d="M19 0h11v26zM11.1 0H0v26zM15 9.6L22.1 26h-4.6l-2.1-5.2h-5.2z"
+                        ></path>
+                    </svg>
+                    <strong>Merch @ Scale Studio</strong>
+                </sp-top-nav-item>
+                <sp-top-nav-item href="#" label="Help" placement="bottom-end">
+                    <sp-icon-help-outline></sp-icon-help-outline>
+                </sp-top-nav-item>
+                <sp-top-nav-item href="#" label="Help" placement="bottom-end">
+                    <sp-icon-bell></sp-icon-bell>
+                    <sp-top-nav-item
+                        href="#"
+                        label="Help"
+                        placement="bottom-end"
+                </sp-top-nav-item>
+                </sp-top-nav-item>
+            </sp-top-nav>
+        `;
+    }
+}
+
+customElements.define('top-nav', TopNav);

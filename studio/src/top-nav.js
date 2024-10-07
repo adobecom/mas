b3ea7750affc0908507e6cda784bc5efaeb48002
Axel Cureno Basurto <axel@adobe.com>
2024-10-07 15:44:44 -0700

    svg as external img

--- studio/src/top-nav.js
+++ studio/src/top-nav.js
@@ -18,6 +18,7 @@ class TopNav extends LitElement {
 
             sp-top-nav-item.logo {
                 color: #eb1000;
+                width: 24px;
             }
 
             sp-top-nav-item strong {
@@ -43,22 +44,7 @@ class TopNav extends LitElement {
                     label="Home"
                     quiet
                 >
-                    <svg
-                        version="1.1"
-                        xmlns="http://www.w3.org/2000/svg"
-                        x="0"
-                        y="0"
-                        viewBox="0 0 30 26"
-                        width="24px"
-                        xml:space="preserve"
-                        role="img"
-                        aria-label="Adobe"
-                    >
-                        <path
-                            fill="#FA0F00"
-                            d="M19 0h11v26zM11.1 0H0v26zM15 9.6L22.1 26h-4.6l-2.1-5.2h-5.2z"
-                        ></path>
-                    </svg>
+                    <img class="logo" src="img/adobe-logo.svg" alt="Adobe" />
                     <strong>Merch @ Scale Studio</strong>
                 </sp-top-nav-item>
                 <sp-top-nav-item href="#" label="Help" placement="bottom-end">

092ea878dbec43cfdc5ad9b6876c7c73a14ad291
Mariia Lukianets <mariia.lukianets@gmail.com>
2024-11-06 12:03:43 +0100

    refresh published fragment

--- studio/src/aem/aem-fragments.js
+++ studio/src/aem/aem-fragments.js
@@ -227,6 +227,13 @@ class AemFragments extends LitElement {
 
     async publishFragment() {
         await this.#aem.sites.cf.fragments.publish(this.fragment);
+        //this.fragment.status = 'PUBLISHED';
+        setTimeout(async () => {
+            const latest = await this.#aem.sites.cf.fragments.getById(
+                this.fragment.id,
+            );
+            aemFragmentCache.get(this.fragment.id)?.refreshFrom(latest);
+        }, 10000);
     }
 
     async deleteFragment() {

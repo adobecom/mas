9764baf111d23bb6bbbdc69f3bccb4b1852b22a5
astatescu <angelostatescu.adobe@gmail.com>
2025-02-04 10:16:13 +0200

    MWPW-167097: Fixed side-nav-support

--- studio/src/mas-side-nav.js
+++ studio/src/mas-side-nav.js
@@ -56,7 +56,7 @@ class MasSideNav extends LitElement {
         }
 
         .side-nav-support {
-            position: absolute;
+            position: fixed;
             bottom: 0;
             width: 220px;
         }

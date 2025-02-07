7bb38ac152e7cfe6f7ad9f62335bc40f77dc3c54
cod23684 <cod23684@adobe.com>
2025-02-07 11:23:32 +0100

    restructure Nala folder

---
+++ nala/studio/ccd/suggested/suggested.page.js
@@ -0,0 +1,22 @@
+export default class SuggestedPage {
+    constructor(page) {
+        this.page = page;
+
+        this.cardIcon = page.locator('merch-icon');
+        this.cardTitle = this.page.locator('h3[slot="heading-xs"]');
+        this.cardEyebrow = page.locator('h4[slot="detail-s"]');
+        this.cardDescription = page
+            .locator('div[slot="body-xs"] p')
+            .first();
+        this.cardLegalLink = page.locator('div[slot="body-xs"] p > a');
+        this.cardPrice = page.locator('p[slot="price"]');
+        this.cardCTA = page.locator('div[slot="cta"] > sp-button');
+        this.cardCTALink = page.locator(
+            'div[slot="cta"] a[is="checkout-link"]',
+        );
+        // this.price = page.locator('span[data-template="price"]');
+        // this.priceStrikethrough = page.locator(
+        //     'span[data-template="strikethrough"]',
+        // );
+    }
+}

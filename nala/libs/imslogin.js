351a6ab83faa4f1d34f9f3715813c9b51bcac1ee
cod23684 <cod23684@adobe.com>
2025-02-12 18:36:53 +0100

    increase timeout

--- nala/libs/imslogin.js
+++ nala/libs/imslogin.js
@@ -16,7 +16,7 @@ async function fillOutSignInForm(props, page) {
     await expect(await page.locator('text=Reset your password')).toBeVisible({ timeout: 45000 }); // Timeout accounting for how long IMS Login page takes to switch form
   }).toPass({
     intervals: [1_000],
-    timeout: 10_000,
+    timeout: 20_000,
   });
 
   heading = await page.locator('.spectrum-Heading1', { hasText: 'Enter your password' }).first().innerText();

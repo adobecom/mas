6a55b0097fdc70e0f959ebac97c3cb78a4190b49
Sean Choi <seanchoi@adobe.com>
2025-02-20 02:23:31 -0700

    Adding hide card logic for mas-content as well.

--- studio/src/mas-recently-updated.js
+++ studio/src/mas-recently-updated.js
@@ -36,9 +36,7 @@ class MasRecentlyUpdated extends LitElement {
                 ${this.loadingIndicator}
                 ${this.fragments.value.map((fragmentStore) => {
                     // Hide the card if the variant isn't one of VARIANTS that is pre-defined.
-                    if (!variantValues.includes(fragmentStore.value.variant))
-                        return html``;
-
+                    if (!variantValues.includes(fragmentStore.value.variant)) return html``;
                     return html`<mas-fragment
                         .store=${fragmentStore}
                         view="render"

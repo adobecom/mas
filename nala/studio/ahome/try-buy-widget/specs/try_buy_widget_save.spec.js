5fc06981ca7babb7f2111aa18a211ca9999a4082
cod23684 <cod23684@adobe.com>
2025-03-06 17:14:05 +0100

    MWPW-169149: typo

--- nala/studio/ahome/try-buy-widget/specs/try_buy_widget_save.spec.js
+++ nala/studio/ahome/try-buy-widget/specs/try_buy_widget_save.spec.js
@@ -10,7 +10,7 @@ export default {
                 price: 'US$79.99/mo',
             },
             browserParams: '#query=',
-            tags: '@mas-studio @ahome @ahome-try-buy-widget @ahome-try-buy-widget-edit @nopr',
+            tags: '@mas-studio @ahome @ahome-try-buy-widget @ahome-try-buy-widget-save @nopr',
         },
         {
             tcid: '1',
@@ -18,10 +18,9 @@ export default {
             path: '/studio.html',
             data: {
                 cardid: '2d9025f7-ea56-4eeb-81b2-a52762358b9d',
-                price: 'US$79.99/mo',
             },
             browserParams: '#query=',
-            tags: '@mas-studio @ahome @ahome-try-buy-widget @ahome-try-buy-widget-edit @nopr',
+            tags: '@mas-studio @ahome @ahome-try-buy-widget @ahome-try-buy-widget-save @nopr',
         },
         {
             tcid: '2',
@@ -29,10 +28,9 @@ export default {
             path: '/studio.html',
             data: {
                 cardid: '2d9025f7-ea56-4eeb-81b2-a52762358b9d',
-                price: 'US$79.99/mo',
             },
             browserParams: '#query=',
-            tags: '@mas-studio @ahome @ahome-try-buy-widget @ahome-try-buy-widget-edit @nopr',
+            tags: '@mas-studio @ahome @ahome-try-buy-widget @ahome-try-buy-widget-save @nopr',
         },
     ],
 };

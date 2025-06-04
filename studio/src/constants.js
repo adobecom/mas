bd93739f3bdb0f57a01fe2fdceff3ec4752a0b99
Predrag Markovic <cod14230@adobe.com>
2025-06-04 12:11:07 +0200

    See all plans cta text option

--- studio/src/constants.js
+++ studio/src/constants.js
@@ -13,7 +13,7 @@ export const CHECKOUT_CTA_TEXTS = {
     'see-more': 'See more',
     'upgrade-now': 'Upgrade now',
     'get-offer': 'Get offer',
-    select: 'Select',
+    'select': 'Select',
     'see-all-plans-and-pricing': 'See all plans & pricing details',
 };
 export const WCS_LANDSCAPE_PUBLISHED = 'PUBLISHED';

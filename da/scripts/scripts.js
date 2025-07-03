ff25dd7030103c70b6397c67c61e30ff063daaef
Mariia Lukianets <mariia.lukianets@gmail.com>
2025-07-03 17:10:53 +0200

    fix formatting

--- da/scripts/scripts.js
+++ da/scripts/scripts.js
@@ -185,7 +185,7 @@ async function loadLazy(doc) {
  * without impacting the user experience.
  */
 function loadDelayed() {
-    window.setTimeout(() => import('../../scripts/delayed.js'), 3000);
+    window.setTimeout(() => import('./delayed.js'), 3000);
     // load anything that can be postponed to the latest here
 }
 

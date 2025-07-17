84e59b90715dba0e264dfb79ae8d5301cbbc2abc
Predrag Markovic <cod14230@adobe.com>
2025-07-17 14:05:21 +0200

    remove unnecessary selection

--- studio/src/studio.js
+++ studio/src/studio.js
@@ -113,13 +113,12 @@ class MasStudio extends LitElement {
     renderCommerceService() {
         const env = this.commerceEnv.value === WCS_ENV_STAGE ? WCS_ENV_STAGE : WCS_ENV_PROD;
         this.commerceService.outerHTML = `<mas-commerce-service env="${env}" locale="${Store.filters.value.locale}"></mas-commerce-service>`;
-        const masCommerceService = document.querySelector('mas-commerce-service');
         function rtePriceProvider(element, options) {
             if (element.dataset.template !== 'legal') return;
             if (!element.getRootNode()?.host?.nodeName === 'RTE-FIELD') return;
             options.displayPlanType = true;
         }
-        masCommerceService.providers.price(rtePriceProvider);
+        this.commerceService.providers.price(rtePriceProvider);
     }
 
     update() {

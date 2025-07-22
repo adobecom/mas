37176efb11c1914b34bccf01362170d450272f8a
Sean Choi <seanchoi@adobe.com>
2025-07-22 11:06:52 -0600

    Update the previous code.

--- io/studio/src/ost-products/write.js
+++ io/studio/src/ost-products/write.js
@@ -75,8 +75,8 @@ const getProducts = async (params) => {
         { locale: 'en_US', landscape: 'PUBLISHED' },
         { locale: 'en_CA', landscape: 'DRAFT' },
         { locale: 'en_CA', landscape: 'PUBLISHED' },
-        { locale: 'en_GB', landscape: 'PUBLISHED' },
-        { locale: 'hi', landscape: 'PUBLISHED' },
+        { locale: 'en_IN', landscape: 'DRAFT' },
+        { locale: 'en_IN', landscape: 'PUBLISHED' },
     ];
     const allProducts = { DRAFT: {}, PUBLISHED: {} };
     const promises = options.map((option) => {

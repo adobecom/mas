7b5656399fdb2bcebbd8766f82b2061e5fb8c3ad
Axel Cureno Basurto <axel@adobe.com>
2025-10-13 14:19:37 -0700

    Update expected URL for Photoshop icon in individuals_save.spec.js to reflect new asset path, ensuring consistency with recent changes in test specifications.

--- nala/studio/acom/plans/individuals/specs/individuals_save.spec.js
+++ nala/studio/acom/plans/individuals/specs/individuals_save.spec.js
@@ -147,7 +147,7 @@ export default {
                     name: 'Photoshop',
                     alt: 'Adobe Photoshop Application',
                     link: 'https://www.adobe.com/products/photoshop.html',
-                    expectedURL: 'https://www.adobe.com/cc-shared/assets/img/product-icons/svg/photoshop.svg',
+                    expectedURL: 'https://www.adobe.com/content/dam/shared/images/product-icons/svg/illustrator.svg',
                 },
             },
             browserParams: '#page=content&path=nala&query=',

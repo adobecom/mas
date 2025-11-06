930d177b037b0bcbd030acd64786414bbb301929
Ilyas Türkben <ilyas@adobe.com>
2025-11-06 15:56:23 +0100

    fix tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,14 @@
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
 
 const ignoresConfig = {
     ignores: [
         '/node_modules/',
         '**/node_modules/**',
+        '/deps/',
+        '**/deps/**',
         '/libs/',
         '**/libs/**',
         '**/dist/**',
@@ -13,7 +18,11 @@ const ignoresConfig = {
     ],
 };
 
-const config = {
+const baseConfig = {
+    plugins: {
+        import: eslintPluginImport,
+        'react-hooks': eslintPluginReactHooks,
+    },
     rules: {
         'prefer-const': 'error',
         'no-var': 'error',
@@ -23,14 +32,46 @@ const config = {
         'no-implicit-globals': 'error',
         'no-undef': 'error',
         'no-global-assign': 'error',
+        'import/prefer-default-export': 'off',
+        'import/no-unresolved': 'off',
+        'import/no-cycle': 'off',
+        'import/no-default-export': 'off',
+        'react-hooks/rules-of-hooks': 'off',
     },
     languageOptions: {
         globals: {
+            ...globals.browser,
+            ...globals.node,
+            ...globals.mocha,
+            ...globals.jest,
+            __ENV: 'readonly',
+            __VU: 'readonly',
+            __ITER: 'readonly',
+            STEP: 'readonly',
+            RAMP: 'readonly',
             window: 'readonly',
             require: 'readonly',
         },
     },
+};
+
+const config = {
     ...eslintPluginPrettierRecommended,
+    plugins: {
+        ...(eslintPluginPrettierRecommended.plugins || {}),
+        ...baseConfig.plugins,
+    },
+    rules: {
+        ...(eslintPluginPrettierRecommended.rules || {}),
+        ...baseConfig.rules,
+    },
+    languageOptions: {
+        ...(eslintPluginPrettierRecommended.languageOptions || {}),
+        globals: {
+            ...(eslintPluginPrettierRecommended.languageOptions?.globals || {}),
+            ...baseConfig.languageOptions.globals,
+        },
+    },
 };
 
 export default [ignoresConfig, config];

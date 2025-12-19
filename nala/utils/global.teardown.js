1512cc4c633274f470758b4ac1a23d0ccc8743da
Ilyas Türkben <ilyas@adobe.com>
2025-12-19 14:06:59 +0100

    remove log

--- nala/utils/global.teardown.js
+++ nala/utils/global.teardown.js
@@ -347,10 +347,7 @@ async function cleanupClonedCards() {
 }
 
 async function globalTeardown() {
-    if (process.env.USE_AUTH !== 'true') {
-        console.info('\n⏩ Skipping Nala Global Teardown (not a Studio test run)\n');
-        return;
-    }
+    if (process.env.USE_AUTH !== 'true') return;
     console.info(`\n---- Executing Nala Global Teardown ----\n`);
     try {
         await cleanupClonedCards();

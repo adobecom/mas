4462ff66047b5f58b7ec2b01a9ad39f3ce0d76ba
Mariia Lukianets <mariia.lukianets@gmail.com>
2026-03-05 18:18:19 +0100

    hardcode ua

--- io/studio/src/common.js
+++ io/studio/src/common.js
@@ -109,6 +109,7 @@ async function fetchOdin(
     const path = `${odinEndpoint}${URI}`;
     const headers = {
         Authorization: `Bearer ${authToken}`,
+        'User-Agent': 'mas-translation-project',
         ...additionalHeaders,
     };
     if (etag) {

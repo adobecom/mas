2aadddf5c7845aafc11dcb62605bdf8f7e22ee94
Mariia Lukianets <mariia.lukianets@gmail.com>
2026-03-06 13:27:51 +0100

    MWPW-188344: test deploy

--- io/studio/src/health-check/index.js
+++ io/studio/src/health-check/index.js
@@ -2,7 +2,7 @@ async function main(params) {
     return {
         statusCode: 200,
         body: {
-            message: 'Hello, world!',
+            message: 'Hello, world',
         },
     };
 }

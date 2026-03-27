729a7ef7260c73ee20abd24083f63a795e9cfe17
Axel Cureno Basurto <axel@adobe.com>
2026-03-27 15:44:23 -0700

    MWPW-183572: point all IO Runtime URLs to dev namespace

--- studio/src/constants.js
+++ studio/src/constants.js
@@ -130,13 +130,13 @@ export const PAGE_NAMES = {
 const isDev = typeof window !== 'undefined' && window.location.hostname === 'localhost';
 const IO_NAMESPACE = isDev ? '14257-merchatscale-axel' : '14257-masstudio';
 
-export const AI_CHAT_BASE_URL = `https://${IO_NAMESPACE}.adobeioruntime.net/api/v1/web/MerchAtScaleStudio`;
-const IO_MCP_NAMESPACE = '14257-merchatscale-axel';
+const IO_DEV_NAMESPACE = '14257-merchatscale-axel';
+export const AI_CHAT_BASE_URL = `https://${IO_DEV_NAMESPACE}.adobeioruntime.net/api/v1/web/MerchAtScaleStudio`;
 export const MCP_SERVER_URL = isDev
     ? 'http://localhost:3001'
-    : `https://${IO_MCP_NAMESPACE}.adobeioruntime.net/api/v1/web/MerchAtScaleMCP`;
-export const IO_MCP_URL = `https://${IO_NAMESPACE}.adobeioruntime.net/api/v1/web/MerchAtScaleMCP`;
-export const KNOWLEDGE_SERVICE_URL = `https://${IO_NAMESPACE}.adobeioruntime.net/api/v1/web/MerchAtScaleKnowledge`;
+    : `https://${IO_DEV_NAMESPACE}.adobeioruntime.net/api/v1/web/MerchAtScaleMCP`;
+export const IO_MCP_URL = `https://${IO_DEV_NAMESPACE}.adobeioruntime.net/api/v1/web/MerchAtScaleMCP`;
+export const KNOWLEDGE_SERVICE_URL = `https://${IO_DEV_NAMESPACE}.adobeioruntime.net/api/v1/web/MerchAtScaleKnowledge`;
 
 export const TAG_STATUS_PUBLISHED = 'mas:status/published';
 export const TAG_STATUS_PUBLISHED_PATH = '/content/cq:tags/mas/status/published';

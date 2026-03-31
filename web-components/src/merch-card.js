fd1456291ab04fe9e3a8974617ce34af93259970
Bozo Jovicic <bozo@hitthecode.com>
2026-03-31 12:13:39 +0200

    MWPW-191268 Merch card stroke color missing when applying a badge

--- web-components/src/merch-card.js
+++ web-components/src/merch-card.js
@@ -257,12 +257,13 @@ export class MerchCard extends LitElement {
 
     updated(changedProperties) {
         if (
-            changedProperties.has('badgeBackgroundColor') ||
-            changedProperties.has('borderColor')
+            this.computedBorderColor &&
+            (changedProperties.has('badgeBackgroundColor') ||
+                changedProperties.has('borderColor'))
         ) {
             this.style.setProperty(
-                '--consonant-merch-card-border',
-                this.computedBorderStyle,
+                '--consonant-merch-card-border-color',
+                this.computedBorderColor,
             );
         }
         if (changedProperties.has('backgroundColor')) {
@@ -297,7 +298,7 @@ export class MerchCard extends LitElement {
         return this.variantLayout.renderLayout();
     }
 
-    get computedBorderStyle() {
+    get computedBorderColor() {
         if (
             ![
                 'ccd-slice',
@@ -307,9 +308,9 @@ export class MerchCard extends LitElement {
                 'full-pricing-express',
             ].includes(this.variant)
         ) {
-            return `1px solid ${
-                this.borderColor ? this.borderColor : this.badgeBackgroundColor
-            }`;
+            return this.borderColor
+                ? this.borderColor
+                : this.badgeBackgroundColor;
         }
         return '';
     }

7ea28da9df4f5b5b95310b69cdd3b13696cc558f
Andrei Tanasa <andreitanasa@Andreis-MacBook-Pro-3.local>
2026-06-11 15:55:50 +0300

    MWPW-186777: Fix prettier

--- studio/src/mas-repository.js
+++ studio/src/mas-repository.js
@@ -960,8 +960,7 @@ export class MasRepository extends LitElement {
 
             const damPath = getDamPath(surfaceKey);
             const locale = this.filters.value.locale;
-            const searchPath =
-                this.page.value === PAGE_NAMES.PROMOTIONS_EDITOR ? damPath : `${damPath}/${locale}`;
+            const searchPath = this.page.value === PAGE_NAMES.PROMOTIONS_EDITOR ? damPath : `${damPath}/${locale}`;
             const searchOptions = {
                 path: searchPath,
                 modelIds: [TAG_MODEL_ID_MAPPING[TAG_MERCH_CARD_COLLECTION]],

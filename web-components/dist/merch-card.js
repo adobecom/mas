073576dba6d1bc45272613ebff983ac7ed1ebad5
Bozo Jovicic <bozo@hitthecode.com>
2026-07-15 18:08:30 +0200

    MWPW-200837 The spacing between the "per license" and "Annual, billed monthly" labels is missing

--- web-components/dist/merch-card.js
+++ web-components/dist/merch-card.js
@@ -6545,7 +6545,7 @@ merch-card[variant="segment"][id] span[data-template="legal"] {
     line-height: 21px;
 }
 
-merch-card[variant="segment"][id] span.price-unit-type {
+merch-card[variant="segment"][id] .price-legal span.price-unit-type {
     display: block;
 }
 

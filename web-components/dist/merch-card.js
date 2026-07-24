acbc705b734716c766ef4541416f21bab31730b1
Joaquin Rivero <jq.rivero@gmail.com>
2026-07-24 10:29:43 -0700

    MWPW-201793: add one-merch-card override without touching multi-card rules

--- web-components/dist/merch-card.js
+++ web-components/dist/merch-card.js
@@ -6272,14 +6272,16 @@ merch-card-collection.product merch-card {
     width: 100%;
   }
 
-.one-merch-card.section merch-card[variant="product"] {
-    margin: 0 auto;
-}
-
+.one-merch-card.section merch-card[variant="product"],
 .two-merch-cards.section merch-card[variant="product"],
 .three-merch-cards.section merch-card[variant="product"],
 .four-merch-cards.section merch-card[variant="product"] {
     width: auto;
+}
+
+.one-merch-card.section merch-card[variant="product"] {
+    width: var(--consonant-merch-card-product-width);
+    margin: 0 auto;
 }
 
 /* grid style for product */

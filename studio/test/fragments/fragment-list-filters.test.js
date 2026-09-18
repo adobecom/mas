3c1e2c0277d2f2356d3fa4feeaacc92a49291a44
fiesta-harness <harness@fiesta.local>
2026-09-18 22:48:35 +0000

    [pinata-code] MWPW-190312: [M@S variations] Allow to filter over variations

--- studio/test/fragments/fragment-list-filters.test.js
+++ studio/test/fragments/fragment-list-filters.test.js
@@ -60,4 +60,58 @@ describe('fragment-list-filters', () => {
 
         expect(filtered).to.have.lengthOf(1);
     });
+
+    it('narrows further by variationPresence on top of the other filters', () => {
+        const stores = [
+            makeStore({ path: '/content/dam/mas/acom/en_US/card-a', getVariations: () => [] }),
+            makeStore({
+                path: '/content/dam/mas/acom/en_US/card-b',
+                getVariations: () => ['/content/dam/mas/acom/en_US/some-code/pzn/card-b'],
+            }),
+        ];
+
+        const filtered = applyFragmentListFilters(stores, {
+            page: PAGE_NAMES.CONTENT,
+            personalizationFilterEnabled: false,
+            variationPresence: 'has-grouped-variation',
+        });
+
+        expect(filtered).to.have.lengthOf(1);
+        expect(filtered[0].get().path).to.equal('/content/dam/mas/acom/en_US/card-b');
+    });
+
+    it('narrows by "Has promo variation" using a sibling promo-variation fragment, then still hides that fragment on CONTENT page', () => {
+        const stores = [
+            makeStore({
+                path: '/content/dam/mas/acom/en_US/promotions/sale/card',
+                tags: [{ id: 'mas:promotion/sale' }],
+                getVariations: () => [],
+            }),
+            makeStore({ path: '/content/dam/mas/acom/en_US/card', getVariations: () => [] }),
+            makeStore({ path: '/content/dam/mas/acom/en_US/card-other', getVariations: () => [] }),
+        ];
+
+        const filtered = applyFragmentListFilters(stores, {
+            page: PAGE_NAMES.CONTENT,
+            personalizationFilterEnabled: false,
+            variationPresence: 'has-promo-variation',
+        });
+
+        expect(filtered).to.have.lengthOf(1);
+        expect(filtered[0].get().path).to.equal('/content/dam/mas/acom/en_US/card');
+    });
+
+    it('leaves the list unchanged when variationPresence is not set', () => {
+        const stores = [
+            makeStore({ path: '/content/dam/mas/acom/en_US/card-a', getVariations: () => [] }),
+            makeStore({ path: '/content/dam/mas/acom/en_US/card-b', getVariations: () => [] }),
+        ];
+
+        const filtered = applyFragmentListFilters(stores, {
+            page: PAGE_NAMES.CONTENT,
+            personalizationFilterEnabled: false,
+        });
+
+        expect(filtered).to.have.lengthOf(2);
+    });
 });

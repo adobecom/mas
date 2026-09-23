f7cec83d575e24a3328b40e204eb6dd9f2aecf53
Joaquin Rivero <jq.rivero@gmail.com>
2026-09-23 16:00:16 -0700

    fix(web-components): hydrate product pricing mapping

--- web-components/src/hydrate.js
+++ web-components/src/hydrate.js
@@ -1,6 +1,7 @@
 import { SELECTOR_MAS_INLINE_PRICE, TRIAL_ANALYTICS_IDS } from './constants.js';
 import { UptLink } from './upt-link.js';
 import { createTag } from './utils.js';
+import { getFragmentMapping } from './variants/variants.js';
 
 const DEFAULT_BADGE_COLOR = '#000000';
 const DEFAULT_BADGE_BACKGROUND_COLOR = '#F8D904';
@@ -1112,9 +1113,8 @@ export async function hydrate(fragment, merchCard) {
     merchCard.variant = variant;
     await merchCard.updateComplete;
 
-    const { aemFragmentMapping: mapping } = merchCard.variantLayout;
-    if (!mapping)
-        throw new Error(`hydrate: variant mapping not found for ${id}`);
+    const mapping = merchCard.variantLayout?.aemFragmentMapping ?? getFragmentMapping(variant);
+    if (!mapping) throw new Error(`hydrate: variant mapping not found for ${id}`);
 
     if (mapping.style === 'consonant') {
         merchCard.setAttribute('consonant', true);
